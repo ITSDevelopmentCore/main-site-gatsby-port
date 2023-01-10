fdc3b0212d6b6a5b92c48cdc786deff85745e3fc
ITS_Razex <itsdevelopmentru@gmail.com>
2023-01-10 06:57:35 +0500

    SSR правки.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,3 @@
-export function onRenderBody({ setHtmlAttributes }) {
-  setHtmlAttributes({ className: "font-['nexa'] text-[18px] laptop:text-[14px] large:text-[16px] desktop:text-[18px] bg-gray-100 dark:bg-[#191E25] dark:text-white" })
+export function onRenderBody({ setBodyAttributes  }) {
+  setBodyAttributes ({ className: "font-['nexa'] text-[18px] laptop:text-[14px] large:text-[16px] desktop:text-[18px] bg-gray-100 dark:bg-[#191E25] dark:text-white" })
 }
