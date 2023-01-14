700fa41cd9a4ec46db932c8a2e9f70626a5426a2
ITS_Razex <itsdevelopmentru@gmail.com>
2023-01-14 22:33:29 +0500

    Файлы релиза.

---
+++ public/545f34e4-86a4fbe1275d796e76a5.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_gatsby_site=self.webpackChunkmy_gatsby_site||[]).push([[980],{2585:function(t,a,e){e.d(a,{Lj:function(){return n}});var l=e(4405);function n(t){return(0,l.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M256 29.816l-231 154v106.368l231-154 231 154V183.816zm0 128.043L105 259.783v90.283l151-101.925 151 101.925v-90.283zm0 112l-87 58.725v67.6l87-58 87 58v-67.6zm0 89.957l-87 58v64.368l87-58 87 58v-64.368z"}}]})(t)}}}]);
+//# sourceMappingURL=545f34e4-86a4fbe1275d796e76a5.js.map
